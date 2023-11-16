b2a5308234b37cbea33d0955317bab31a6990a6b
Nathan Soussana <nathan.soussana@gmail.com>
2023-11-16 20:24:26 +0100

    Created basic styles

--- src/jsx/App.jsx
+++ src/jsx/App.jsx
@@ -2,7 +2,10 @@ export default function App() {
 
   return (
     <>
-      <h1>Hello</h1>
+      <h1>Hello World!</h1>
+      <h2>Hello World!</h2>
+      <h3>Hello World!</h3>
+      <p>Hello World!</p>
     </>
   )
 }
