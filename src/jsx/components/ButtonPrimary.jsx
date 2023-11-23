b5f22317524f8cd7bffe812147e30049baa4e77f
Nathan Soussana <nathan.soussana@gmail.com>
2023-11-23 12:13:14 +0100

    Removed target _blank on internal link (primary btns)

--- src/jsx/components/ButtonPrimary.jsx
+++ src/jsx/components/ButtonPrimary.jsx
@@ -3,8 +3,6 @@ export default function ButtonPrimary({ text, link }) {
     <a 
       className="btn btn-primary" 
       href={link}
-      target="_blank"
-      rel="noopener noreferrer"
     >
       {text}
     </a>
