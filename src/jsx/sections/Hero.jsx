d88819a18c43d1d2deb4342690857fc1dabbac1e
Nathan Soussana <nathan.soussana@gmail.com>
2023-11-27 15:58:39 +0100

    Made internal CTAs functional

--- src/jsx/sections/Hero.jsx
+++ src/jsx/sections/Hero.jsx
@@ -7,7 +7,7 @@ export default function Hero() {
           <div className="hero-content">
             <h1 className="hero-content-title">Nice to meet you!</h1>
             <p className="hero-content-text">My name is Nathan Soussana. I&apos;m a front-end developer based in Zürich, Switzlerand, passionate about building clean and beautiful web apps that users love. I focus mostly on React and JavaScript.</p>
-            <ButtonPrimary text="View Work" link="#" />
+            <ButtonPrimary text="View Work" link="#work" />
           </div>
           <img src="assets/profile_picture.jpeg" alt="Profile Picture" className="hero-image" />
         </div>
